6974cfd10fbae5c6030c9f41829dca4fa7f0a33b
Henrique Bissoli Malaman Alonso <bissoli@3stamina.com>
2023-03-13 03:29:55 -0300

    Criada e configurada a classe e header numOps, responsável por imprimir a quantidade de operações necessárias para executar o código

---
+++ numOps.h
@@ -0,0 +1,12 @@
+//
+// Created by Bissoli on 13/03/23.
+//
+
+#ifndef MAIOR_E_MENOR_ELEMENTO_DE_UM_ARRAY_NUMOPS_H
+#define MAIOR_E_MENOR_ELEMENTO_DE_UM_ARRAY_NUMOPS_H
+
+int numOperations;
+
+void showOperations(int numOperations);
+
+#endif //MAIOR_E_MENOR_ELEMENTO_DE_UM_ARRAY_NUMOPS_H
